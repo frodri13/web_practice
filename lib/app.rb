f069246f8da21e53628df08228b9c0b1963fbf66
Fabio <alessf13@gmail.com>
2021-06-07 16:18:20 +0100

    sinatra-contrib gem installed and added to gemfile

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/' do
   "<h1>Hello World</h1>"
+end
+
+get '/secret' do
+  "What are you doing here? Don't tell anyone!"
 end
 
 get '/bye' do
